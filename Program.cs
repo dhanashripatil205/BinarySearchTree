07e494a5d2f819b6e39400bfc1984673587fd565
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-13 17:17:55 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,18 +6,8 @@ namespace BinarySearchTree
     {
         public static void Main(string[] args)
         {
-            Binarytree bn = new Binarytree();
-            bn.Add(30);
-            bn.Add(40);
-            bn.Add(50);
-            bn.Add(60);
-            bn.Add(80);
-                Console.WriteLine("After Preorder Result Will be: ");
-            bn.TravesrePreorder(bn.root);
-                Console.WriteLine("After Postorder Result Will be: ");
-            bn.TravesrePostorder(bn.root);
-                Console.WriteLine("After Inorder Result Will be: ");
-            bn.TravesreInorder(bn.root);
+                       Console.WriteLine("***** Welcome to Binary Search Tree Program *****");
+
         }
 
     }
